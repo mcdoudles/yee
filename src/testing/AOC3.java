07760b1b62fccb6f9c217f368daf8defb105e6ec
Tobias <my@email.com>
2020-12-03 18:35:32 +0100

    yee

--- src/testing/AOC3.java
+++ src/testing/AOC3.java
@@ -7,24 +7,7 @@ import java.util.List;
 
 public class AOC3 {
 
-	public static int task1(List<String> str) {
-		int result = 0;
-		int spalte = 3;
-		for (int i = 1; i < str.size(); i = i + 1) {
-			if (str.get(i).charAt(spalte) == "#".charAt(0)) {
-				System.out.print(i + ", " + spalte + " " + str.get(i) + "\n");
-				result++;
-			}
-			if (spalte + 3 > 30) {
-				spalte = spalte - 28;
-			} else {
-				spalte = spalte + 3;
-			}
-		}
-		return result;
-	}
-
-	public static int task2(List<String> str, int right, int down) {
+	public static long task2(List<String> str, int right, int down) {
 		int result = 0;
 		int spalte = right;
 		for (int i = down; i < str.size(); i = i + down) {
@@ -42,7 +25,8 @@ public class AOC3 {
 
 	public static void main(String args[]) throws IOException {
 		List<String> str = Files.readAllLines(Paths.get("C:\\Git\\testing\\input\\input day 3.txt"));
-		System.out.print("task1 = " + task2(str, 3, 1) + "\ntask2 = " + task2(str, 1, 1) + " * " + task2(str, 3, 1) + " * "
-				+ task2(str, 5, 1) + " * " + task2(str, 7, 1) + " * " + task2(str, 1, 2) + " = ");
+		System.out.print("task1 = " + task2(str, 3, 1));
+		System.out.print("\ntask2 = "
+				+ task2(str, 1, 1) * task2(str, 3, 1) * task2(str, 5, 1) * task2(str, 7, 1) * task2(str, 1, 2));
 	}
 }
